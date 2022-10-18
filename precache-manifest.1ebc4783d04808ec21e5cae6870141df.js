3cae5e31605056a126c5a5e23b94fa1104e99852
DontiRohit <65717106+DontiRohit@users.noreply.github.com>
2022-10-18 21:01:01 +0530

    Updates

---
+++ precache-manifest.1ebc4783d04808ec21e5cae6870141df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b74f4da972ca1d567b0",
+    "url": "/text_utils/static/css/main.86673cc4.chunk.css"
+  },
+  {
+    "revision": "0b74f4da972ca1d567b0",
+    "url": "/text_utils/static/js/main.0b74f4da.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/text_utils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/text_utils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "22c3d67f433ab2dfb56e",
+    "url": "/text_utils/static/js/runtime~main.22c3d67f.js"
+  },
+  {
+    "revision": "0560437b15cfa37f8a2ff460750ed3e8",
+    "url": "/text_utils/index.html"
+  }
+];
